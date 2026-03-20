2ab249818e92bb1367733c71d0495211c08cad06
Yash-verma2 <yash04523@gmail.com>
2026-03-20 12:11:00 +0530

    logo updated

--- sw.js
+++ sw.js
@@ -3,11 +3,11 @@ const ASSETS_TO_CACHE = [
   'index.html',
   'manifest.json',
   'assets/branding/favicon.ico',
-  'assets/branding/favicon-16.png',
-  'assets/branding/favicon-32.png',
-  'assets/branding/icon-192.png',
-  'assets/branding/icon-512.png',
-  'assets/branding/apple-touch-icon.png'
+  'assets/branding/favicon-16x16.png',
+  'assets/branding/favicon-32x32.png',
+  'assets/branding/favicon-192x192.png',
+  'assets/branding/favicon-512x512.png',
+  'assets/branding/favicon-180x180.png'
 ];
 
 self.addEventListener('install', (event) => {

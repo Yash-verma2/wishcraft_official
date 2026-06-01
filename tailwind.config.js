7a8a355349e3b1919535e5ef60aaced36e060630
Yash-verma2 <yash04523@gmail.com>
2026-06-01 11:00:08 +0530

    ad removed

---
+++ tailwind.config.js
@@ -0,0 +1,38 @@
+module.exports = {
+  content: [
+    "./*.html",
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+        display: ['Outfit', 'sans-serif'],
+      },
+      colors: {
+        brand: {
+          50: '#fff1f2',
+          100: '#ffe4e6',
+          500: '#f43f5e',
+          600: '#e11d48',
+          700: '#be123c',
+        }
+      },
+      animation: {
+        float: 'float 3s ease-in-out infinite',
+        shimmer: 'shimmer 2s linear infinite',
+      },
+      keyframes: {
+        float: {
+          '0%, 100%': { transform: 'translateY(0)' },
+          '50%': { transform: 'translateY(-10px)' },
+        },
+        shimmer: {
+          '0%': { backgroundPosition: '-200% 0' },
+          '100%': { backgroundPosition: '200% 0' },
+        }
+      }
+    },
+  },
+  plugins: [],
+}
